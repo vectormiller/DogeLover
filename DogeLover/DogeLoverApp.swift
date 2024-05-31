e9ceada6b64a08a5c94d26b164521b8758e3da30
Victor Glebov <victorglebovv@yandex.ru>
2024-05-31 13:32:47 +0600

    Initial Commit

---
+++ DogeLover/DogeLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogeLoverApp.swift
+//  DogeLover
+//
+//  Created by vectormiller on 5/31/24.
+//
+
+import SwiftUI
+
+@main
+struct DogeLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
